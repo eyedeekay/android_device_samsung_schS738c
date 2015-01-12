377e9cecbf79a18a298d8e76c549ce92cfef9b9a
cmotc <cmotc@openmailbox.org>
2015-01-11 20:49:01 -0500

    sync

--- device_schS738c.mk
+++ device_schS738c.mk
@@ -1,35 +1,18 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/schS738c/AndroidBoardVendor.mk)
-
 DEVICE_PACKAGE_OVERLAYS += device/samsung/schS738c/overlay
-
 LOCAL_PATH := device/samsung/schS738c
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES = \
     $(LOCAL_KERNEL):kernel
-
-$(call inherit-product, build/target/product/full.mk)
-#$(call inherit-product, vendor/samsung/schS738c/AndroidBoardVendor.mk)
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_schS738c
 PRODUCT_DEVICE := schS738c
-
-#PRODUCT_PACKAGES += \
-#	schS738c.fstab \
-#	init.qcom.rc \
-#	init.qcom.usb.rc \
 
 ## Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -136,6 +119,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vold.umsdirtyratio=50 \
 	persist.sys.vold.switchablepair=sdcard0,sdcard1
 
+#Camera
+#PRODUCT_COPY_FILES += \
+#	device/samsung/msm7x27a-common/camera/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so
+
 ## Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
@@ -211,3 +198,10 @@ PRODUCT_PACKAGES += \
 	make_ext4fs \
 	setup_fs \
 	com.android.future.usb.accessory
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/full.mk)
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product-if-exists, vendor/samsung/schS738c/AndroidBoardVendor.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
