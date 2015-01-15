cb2a37055b2c017ffe9ed7301e49e6d3c1f2faa7
cmotc <cmotc@openmailbox.org>
2015-01-15 00:58:41 -0500

    enabled the camera, having a problem building with gralloc

--- device_schS738c.mk
+++ device_schS738c.mk
@@ -120,8 +120,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.vold.switchablepair=sdcard0,sdcard1
 
 #Camera
-#PRODUCT_COPY_FILES += \
-#	device/samsung/msm7x27a-common/camera/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so
+PRODUCT_COPY_FILES += \
+	device/samsung/msm7x27a-common/camera/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so
 
 ## Permissions
 PRODUCT_COPY_FILES += \
