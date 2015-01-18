94a76648ded56855d87567e1be6d035a6acad27c
cmotc <cmotc@openmailbox.org>
2015-01-17 20:51:19 -0500

    Undoing last experiment

--- cm.mk
+++ cm.mk
@@ -1,9 +1,9 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := schS738c
 LOCAL_PATH := $(call my-dir)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
