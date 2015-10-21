e50ef94185e0369d08b8fdc5769c1e71854fb4c0
dyngar <dyngar@dyngar.local>
2015-10-21 02:05:54 -0400

    planned out conditionals for universal ROM makefile / 20151021

--- rom_schS738c.mk
+++ rom_schS738c.mk
@@ -5,8 +5,18 @@ LOCAL_PATH := $(call my-dir)
 # Specify phone tech before including full_phone
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+#conditional determines if it's a CM tree
+# Inherit some common CM stuff.
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#else conditional determines if it's an Omni tree
 # Inherit some common omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+MY_ROM_PREFIX=omni
+#else conditional determines if it's an AOKP tree
+#else conditional determines if it's an AOSP tree
+#else conditional determines if it's a Guardian ROM tree
+#else conditional determines if it's a Replicant tree
+
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/schS738c/device_schS738c.mk)
@@ -20,7 +30,7 @@ endif
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := schS738c
-PRODUCT_NAME := omni_schS738c
+PRODUCT_NAME := $(MY_ROM_PREFIX)_schS738c
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := schS738c
 PRODUCT_MANUFACTURER := samsung
