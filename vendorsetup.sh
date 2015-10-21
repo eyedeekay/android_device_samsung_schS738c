0d6a41839f9005c45ae45ac6c7f416e800466817
dyngar <dyngar@dyngar.local>
2015-10-21 15:22:30 -0400

    removed referencess to cyanogenmod from vendorsetup / 20151021

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo cm_schS738c-userdebug
-add_lunch_combo cm_schS738c-eng
+#add_lunch_combo cm_schS738c-userdebug
+#add_lunch_combo cm_schS738c-eng
 add_lunch_combo omni_schS738c-userdebug
 add_lunch_combo omni_schS738c-eng
