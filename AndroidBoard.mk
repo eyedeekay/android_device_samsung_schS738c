6801117bf6b300dde06944f290e8ef4e524cb8ee
dyngar <dyngar@dyngar.local>
2015-10-21 02:26:40 -0400

    oh man I might actually be able to figure out how to get this thing working again. / 20151021

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -20,4 +20,5 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 #-include vendor/samsung/schS738c/AndroidBoardVendor.mk
+include vendor/samsung/schS738c/AndroidBoardVendor.mk
 $(call inherit-product-if-exists, vendor/samsung/schS738c/AndroidBoardVendor.mk)
